b5b84eeb9c085543d35ecfa3b3e8f486b69c59e4
Anubhav Aryal <aryalanubhav@gmail.com>
2022-05-31 17:18:03 -0500

    created skeleton of api to implement later

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from flask import Flask
 from google.cloud import language_v1
 import neuralcoref
 import os
@@ -8,6 +9,7 @@ import requests
 import spacy
 import time
 
+app = Flask(__name__)
 
 # class to store each Discord message
 class Message:
@@ -47,6 +49,58 @@ encoding_type = language_v1.EncodingType.UTF8
 
 # dict of users for their respective user_id's
 users = {}
+
+# if the app is currently running
+running = False
+
+
+@app.route("/start")
+def start_analysis():
+    global running
+    running = True
+    return "started analysis"
+
+
+@app.route("/stop")
+def stop_analysis():
+    global running
+    running = False
+    return "stopped analysis"
+
+
+@app.route("/pog")
+def pog():
+    return str(running)
+
+
+@app.route("/sentiment/message")
+def load_message_sentiments():
+    return "a bunch of message sentiments"
+
+
+@app.route("/sentiment/message/user")
+def load_message_sentiments_for_user():
+    return "a bunch of message sentiments for specific user"
+
+
+@app.route("/sentiment/entity")
+def load_entity_sentiments():
+    return "a bunch of entity sentiments"
+
+
+@app.route("/sentiment/entity/user")
+def load_entity_sentiments_for_user():
+    return "a bunch of entity sentiments for specific user"
+
+
+@app.route("/sentiment/entity/inverse")
+def load_entity_sentiments_inverse():
+    return "a bunch of entity sentiments but inverted"
+
+
+@app.route("/sentiment/message/user/polarized")
+def load_min_max_message_sentiments():
+    return "the minimum and maximum sentiment message for user"
 
 
 # returns json associated with request to discord api
