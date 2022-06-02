956f3b29c635047a0f4462aff3708c57dd9b86c7
Anubhav Aryal <aryalanubhav@gmail.com>
2022-06-02 00:49:51 -0500

    changed resolve_coreferences function to use database

--- backend/harmony/analyzer.py
+++ backend/harmony/analyzer.py
@@ -4,13 +4,14 @@ from harmony.helpers import add_user, prepare_message, send_request
 from harmony.models import Channel, CorefMessage, Message, MessageCluster, MessageSentiment, User, UserSentiment
 from google.cloud import language_v1
 import neuralcoref
+import re
 import spacy
 
 # TODO: commit all db commands (not in loop but at very end so all commands get ran at once for maximum optimialness)
 
 # load nlp
-# nlp = spacy.load('en_core_web_sm')
-# neuralcoref.add_to_pipe(nlp, blacklist=False)
+nlp = spacy.load('en_core_web_sm')
+neuralcoref.add_to_pipe(nlp, blacklist=False)
 
 # # instantiate google client
 # client = language_v1.LanguageServiceClient()
@@ -32,6 +33,7 @@ class Analyzer:
         # add channel to database
         channel = Channel(id=self.channel_id, running=True, stage=0, progress=0)
         db.session.add(channel)
+        db.session.commit()
 
         # store messages
         print("getting messages")
@@ -42,6 +44,11 @@ class Analyzer:
         print("clustering messages")
         self.create_clusters()
         print("finished clustering messages")
+
+        # coreference resolution
+        print("starting coref")
+        self.resolve_coreferences()
+        print("finished coref")
 
 
     # stores all messages in the channel in the database
@@ -76,7 +83,7 @@ class Analyzer:
         db.session.commit()
 
 
-    # creates clusters of messages based on time frame to prepare for coreference resolution
+    # creates message clusters based on time frame to prepare for coreference resolution
     def create_clusters(self):
         first_time = None  # time of first message in the cluster
         last_time = None  # time of last message in the cluster
@@ -124,116 +131,138 @@ class Analyzer:
         db.session.commit()
 
 
-    # # resolves coreferences in message clusters
-    # def resolve_coreferences(clusters):
-    #     # messages after updated with coreference resolution
-    #     messages = []
+    # resolves coreferences in message clusters
+    def resolve_coreferences(self):
+        user_pattern = re.compile(r"^.+ said $")  # pattern matches "username said "
 
-    #     # pattern matches "username said "
-    #     user_pattern = re.compile(r"^.+ said $")
+        # get all messages for this channel
+        messages = CorefMessage.query.join(MessageCluster, CorefMessage.cluster_id == MessageCluster.id)\
+            .join(Message, CorefMessage.message_id == Message.id)\
+            .join(User, Message.user_id == User.id)\
+            .filter(MessageCluster.channel_id == self.channel_id)\
+            .add_columns(MessageCluster.id, User.username)\
+            .all()
+        
+        prev_id = None  # stores id of previous message cluster
+        
+        # combine all messages in cluster to assist with coreference resolution
+        cluster_message = u""
 
-    #     for cluster in clusters:
-    #         # combine all messages in cluster to assist with coreference resolution
-    #         cluster_message = u""
+        for (message, id, username) in messages:
+            if prev_id is None:
+                prev_id = id
+            elif prev_id != id:
+                # resolve coreferences
+                doc = nlp(cluster_message)
 
-    #         for message in cluster:
-    #             # prepare message
-    #             content = users[message.user_id]  + ' said "' + message.content.replace('"', '\'') + '." '
-    #             cluster_message += content
+                # update coref messages
+                coref_contents = [i[:-1] for i in doc._.coref_resolved.split('"') if not user_pattern.match(i)]
+                del coref_contents[-1]
+
+                coref_messages = CorefMessage.query.filter_by(cluster_id=prev_id)
+                for (i, coref_message) in enumerate(coref_messages):
+                    coref_message.content = coref_contents[i]
+
+                # reset variables
+                cluster_message = u""
+                prev_id = id
+
+            # prepare message by surrounding in quotes and prepending it with "(username) said"
+            content = username  + ' said "' + message.content.replace('"', '\'') + '." '
+            cluster_message += content
+
+        # resolve final cluster
+        doc = nlp(cluster_message)
+
+        coref_contents = [i[:-1] for i in doc._.coref_resolved.split('"') if not user_pattern.match(i)]
+        del coref_contents[-1]
+
+        coref_messages = CorefMessage.query.filter_by(cluster_id=prev_id)
+        for (i, coref_message) in enumerate(coref_messages):
+            coref_message.content = coref_contents[i]
+
+        db.session.commit()
+
+
+
+    # sentiment analysis on entities and messages
+    def analyze_sentiments(messages, original_messages):
+        print("DOING SENTIMENT ANALYSIS")
+        # sentiment of this user referring to other users (sentiment of other users referring to this user can be derived from this dict)
+        entity_sentiments = {}
+
+        # sentiment of each message (user min/max sentiments can be derived from this dict)
+        message_sentiments = {}
+
+        # stores content of document
+        content = ""
+
+
+        # finds the message containing the given mention
+        def find_message(mention):
+            offset = mention.text.begin_offset
+            dist = 2  # distance between two messages in the "content" string (each message is separated with 2 characters, ". ")
+
+            for (message, original_message) in zip(messages, original_messages):
+                # if the mention is contained within the current message, return it
+                if len(message.content) > offset:
+                    return original_message
+
+                # move to next message
+                offset -= len(message.content) + dist
+        
+
+        # computes entity and message sentiments for content
+        def compute_sentiments(content):
+            # ensure that content is contained in a single unit (one API unit is < 1000 characters)
+            assert len(content) < 1000
+
+            # create document
+            document = {'content': content, 'type_': type_, 'language': language}
+
+            sentiment_response = client.analyze_sentiment(request={'document': document, 'encoding_type': encoding_type})
+
+            for sentence in sentiment_response.sentences:
+                # find message using span of sentence
+                sentence_message = find_message(sentence)
+
+                # initialize dict value if it doesnt yet exist
+                if users[sentence_message.user_id] not in message_sentiments:
+                    message_sentiments[users[sentence_message.user_id]] = {}
+
+                # update message sentiments
+                message_sentiments[users[sentence_message.user_id]][sentence_message] = (sentence.sentiment.score, sentence.sentiment.magnitude)
             
-    #         # resolve coreferences
-    #         doc = nlp(cluster_message)
+            entity_response = client.analyze_entity_sentiment(request={'document': document, 'encoding_type': encoding_type})
 
-    #         # dissolve cluster message
-    #         new_messages = [i[:-1] for i in doc._.coref_resolved.split('"') if not user_pattern.match(i)]
-    #         del new_messages[-1]
+            for entity in entity_response.entities:
+                # find message using span of entity
+                for mention in entity.mentions:
+                    entity_message = find_message(mention)
 
-    #         for (message, new_message) in zip(cluster, new_messages):
-    #             messages.append(Message(new_message, message.message_id, message.user_id, message.timestamp))
-        
-    #     # original messages passed to function
-    #     original_messages = [message for cluster in clusters for message in cluster]  # flatten cluster
-    #     return messages, original_messages
-
-
-    # # sentiment analysis on entities and messages
-    # def analyze_sentiments(messages, original_messages):
-    #     print("DOING SENTIMENT ANALYSIS")
-    #     # sentiment of this user referring to other users (sentiment of other users referring to this user can be derived from this dict)
-    #     entity_sentiments = {}
-
-    #     # sentiment of each message (user min/max sentiments can be derived from this dict)
-    #     message_sentiments = {}
-
-    #     # stores content of document
-    #     content = ""
-
-
-    #     # finds the message containing the given mention
-    #     def find_message(mention):
-    #         offset = mention.text.begin_offset
-    #         dist = 2  # distance between two messages in the "content" string (each message is separated with 2 characters, ". ")
-
-    #         for (message, original_message) in zip(messages, original_messages):
-    #             # if the mention is contained within the current message, return it
-    #             if len(message.content) > offset:
-    #                 return original_message
-
-    #             # move to next message
-    #             offset -= len(message.content) + dist
-        
-
-    #     # computes entity and message sentiments for content
-    #     def compute_sentiments(content):
-    #         # ensure that content is contained in a single unit (one API unit is < 1000 characters)
-    #         assert len(content) < 1000
-
-    #         # create document
-    #         document = {'content': content, 'type_': type_, 'language': language}
-
-    #         sentiment_response = client.analyze_sentiment(request={'document': document, 'encoding_type': encoding_type})
-
-    #         for sentence in sentiment_response.sentences:
-    #             # find message using span of sentence
-    #             sentence_message = find_message(sentence)
-
-    #             # initialize dict value if it doesnt yet exist
-    #             if users[sentence_message.user_id] not in message_sentiments:
-    #                 message_sentiments[users[sentence_message.user_id]] = {}
-
-    #             # update message sentiments
-    #             message_sentiments[users[sentence_message.user_id]][sentence_message] = (sentence.sentiment.score, sentence.sentiment.magnitude)
-            
-    #         entity_response = client.analyze_entity_sentiment(request={'document': document, 'encoding_type': encoding_type})
-
-    #         for entity in entity_response.entities:
-    #             # find message using span of entity
-    #             for mention in entity.mentions:
-    #                 entity_message = find_message(mention)
-
-    #                 # initialize dict value if it doesnt yet exist
-    #                 if users[entity_message.user_id] not in entity_sentiments:
-    #                     entity_sentiments[users[entity_message.user_id]] = {}
+                    # initialize dict value if it doesnt yet exist
+                    if users[entity_message.user_id] not in entity_sentiments:
+                        entity_sentiments[users[entity_message.user_id]] = {}
                     
-    #                 if entity.name not in entity_sentiments[users[entity_message.user_id]]:
-    #                     entity_sentiments[users[entity_message.user_id]][entity.name] = {}
+                    if entity.name not in entity_sentiments[users[entity_message.user_id]]:
+                        entity_sentiments[users[entity_message.user_id]][entity.name] = {}
                     
-    #                 # update entity sentiments
-    #                 entity_sentiments[users[entity_message.user_id]][entity.name][entity_message] = (mention.sentiment.score, mention.sentiment.magnitude)
+                    # update entity sentiments
+                    entity_sentiments[users[entity_message.user_id]][entity.name][entity_message] = (mention.sentiment.score, mention.sentiment.magnitude)
 
 
-    #     for message in messages:
-    #         if len(message.content) + len(content) < 1000:
-    #             # add message to current group if character limit not reached
-    #             content += message.content + ". "
-    #         else:
-    #             # compute unit of messages
-    #             compute_sentiments(content)
-    #             content = ""
+        for message in messages:
+            if len(message.content) + len(content) < 1000:
+                # add message to current group if character limit not reached
+                content += message.content + ". "
+            else:
+                # compute unit of messages
+                compute_sentiments(content)
+                content = ""
         
-    #     # compute last unit of messages
-    #     if len(content) > 0:
-    #         compute_sentiments(content)
-    #         content = ""
+        # compute last unit of messages
+        if len(content) > 0:
+            compute_sentiments(content)
+            content = ""
         
-    #     return entity_sentiments, message_sentiments
+        return entity_sentiments, message_sentiments
