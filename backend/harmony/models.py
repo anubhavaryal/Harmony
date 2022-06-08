ba0c6536c76906200e68b27a53eb5bf11092b651
anubhavaryal <aryalanubhav@gmail.com>
2022-06-08 04:41:20 -0500

    added user alternates and foreign key constraints to models

--- backend/harmony/models.py
+++ backend/harmony/models.py
@@ -3,8 +3,8 @@ from harmony import db
 
 # bridge entity used for many-many relationship between channels and users
 user_bridge_association = db.Table('users',
-    db.Column('user_id', db.String(32), db.ForeignKey('user.id'), primary_key=True),
-    db.Column('channel_id', db.String(32), db.ForeignKey('channel.id'), primary_key=True)
+    db.Column('user_id', db.String(32), db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('channel_id', db.String(32), db.ForeignKey('channel.id', ondelete='CASCADE'), primary_key=True)
 )
 
 
@@ -15,10 +15,12 @@ class Channel(db.Model):
     running = db.Column(db.Boolean, nullable=False)  # whether the channel is currently being analyzed
     stage = db.Column(db.Integer, nullable=False)  # the current analysis stage
     progress = db.Column(db.Integer, nullable=False)  # the progress in the current stage
+    limit = db.Column(db.Integer, nullable=False)  # max number of messages to analyze
 
-    users = db.relationship('User', secondary=user_bridge_association, back_populates='channels', lazy='dynamic')
-    messages = db.relationship('Message', back_populates='channel')
-    clusters = db.relationship('MessageCluster', back_populates='channel')
+    users = db.relationship('User', secondary=user_bridge_association, back_populates='channels', lazy='dynamic', cascade='all, delete')
+    messages = db.relationship('Message', back_populates='channel', cascade='all, delete', passive_deletes=True)
+    clusters = db.relationship('MessageCluster', back_populates='channel', cascade='all, delete', passive_deletes=True)
+    user_alternates = db.relationship('UserAlternate', back_populates='channel', lazy='dynamic', cascade='all, delete', passive_deletes=True)
 
 
 # a Discord user
@@ -27,52 +29,71 @@ class User(db.Model):
 
     username = db.Column(db.String(32), nullable=False)
 
-    channels = db.relationship('Channel', secondary=user_bridge_association, back_populates='users')
-    messages = db.relationship('Message', back_populates='user')
-    object_sentiments = db.relationship('UserSentiment', foreign_keys='UserSentiment.object_user_id', back_populates='object_user')
-    subject_sentiments = db.relationship('UserSentiment', foreign_keys='UserSentiment.subject_user_id', back_populates='subject_user')
+    channels = db.relationship('Channel', secondary=user_bridge_association, back_populates='users', passive_deletes=True)
+    messages = db.relationship('Message', back_populates='user', cascade='all, delete', passive_deletes=True)
+    object_sentiments = db.relationship('UserSentiment', foreign_keys='UserSentiment.object_user_id', back_populates='object_user', cascade='all, delete', passive_deletes=True)
+    subject_sentiments = db.relationship('UserSentiment', foreign_keys='UserSentiment.subject_user_id', back_populates='subject_user', cascade='all, delete', passive_deletes=True)
+    alternates = db.relationship('UserAlternate', back_populates='user', cascade='all, delete', passive_deletes=True)
+
+
+# an alternate name for the user
+class UserAlternate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    channel_id = db.Column(db.String(32), db.ForeignKey('channel.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.String(32), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+    name = db.Column(db.Text, nullable=False)  # alternate name
+
+    channel = db.relationship('Channel', back_populates='user_alternates')
+    user = db.relationship('User', back_populates='alternates')
+
+    def to_json(self):
+        return {
+            'user_id': self.user_id,
+            'name': self.name
+        }
 
 
 # a group of messages that share the same channel and were sent around the same time frame
 class MessageCluster(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    channel_id = db.Column(db.String(32), db.ForeignKey('channel.id'), nullable=False)
+    channel_id = db.Column(db.String(32), db.ForeignKey('channel.id', ondelete='CASCADE'), nullable=False)
 
     channel = db.relationship('Channel', back_populates='clusters')
-    messages = db.relationship('ClusterMessage', back_populates='cluster')
+    messages = db.relationship('ClusterMessage', back_populates='cluster', cascade='all, delete', passive_deletes=True)
 
 
 # a Discord message
 class Message(db.Model):
     id = db.Column(db.String(32), primary_key=True)
-    channel_id = db.Column(db.String(32), db.ForeignKey('channel.id'), nullable=False)
-    user_id = db.Column(db.String(32), db.ForeignKey('user.id'), nullable=False)
+    channel_id = db.Column(db.String(32), db.ForeignKey('channel.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.String(32), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.Text, nullable=False)
 
     channel = db.relationship('Channel', back_populates='messages')
     user = db.relationship('User', back_populates='messages')
-    cluster_message = db.relationship('ClusterMessage', back_populates='message', uselist=False)
-    message_sentiment = db.relationship('MessageSentiment', back_populates='message', uselist=False)
-    user_sentiments = db.relationship('UserSentiment', back_populates='message')
+    cluster_message = db.relationship('ClusterMessage', back_populates='message', uselist=False, cascade='all, delete', passive_deletes=True)
+    message_sentiment = db.relationship('MessageSentiment', back_populates='message', uselist=False, cascade='all, delete', passive_deletes=True)
+    user_sentiments = db.relationship('UserSentiment', back_populates='message', cascade='all, delete', passive_deletes=True)
 
 
 # a message that belongs to a cluster
 class ClusterMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.String(32), db.ForeignKey('message.id'), nullable=False)
-    message_cluster_id = db.Column(db.Integer, db.ForeignKey('message_cluster.id'), nullable=False)
+    message_id = db.Column(db.String(32), db.ForeignKey('message.id', ondelete='CASCADE'), nullable=False)
+    message_cluster_id = db.Column(db.Integer, db.ForeignKey('message_cluster.id', ondelete='CASCADE'), nullable=False)
 
     message = db.relationship('Message', back_populates='cluster_message')
-    coref_message = db.relationship('CorefMessage', back_populates='cluster_message', uselist=False)
+    coref_message = db.relationship('CorefMessage', back_populates='cluster_message', uselist=False, cascade='all, delete', passive_deletes=True)
     cluster = db.relationship('MessageCluster', back_populates='messages')
 
 
 # a message after it has had its coreferences resolved
 class CorefMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cluster_message_id = db.Column(db.Integer, db.ForeignKey('cluster_message.id'), nullable=False)
+    cluster_message_id = db.Column(db.Integer, db.ForeignKey('cluster_message.id', ondelete='CASCADE'), nullable=False)
 
     content = db.Column(db.Text, nullable=False)  # content of the message after coreference resolution
 
@@ -84,7 +105,7 @@ class CorefMessage(db.Model):
 # the sentiment of a message
 class MessageSentiment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.String(32), db.ForeignKey('message.id'), nullable=False)
+    message_id = db.Column(db.String(32), db.ForeignKey('message.id', ondelete='CASCADE'), nullable=False)
 
     score = db.Column(db.Float, nullable=False)
     magnitude = db.Column(db.Float, nullable=False)
@@ -95,9 +116,9 @@ class MessageSentiment(db.Model):
 # the sentiment of the users present in the message contents
 class UserSentiment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.String(32), db.ForeignKey('message.id'), nullable=False)
-    object_user_id = db.Column(db.String(32), db.ForeignKey('user.id'), nullable=False)
-    subject_user_id = db.Column(db.String(32), db.ForeignKey('user.id'), nullable=False)
+    message_id = db.Column(db.String(32), db.ForeignKey('message.id', ondelete='CASCADE'), nullable=False)
+    object_user_id = db.Column(db.String(32), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    subject_user_id = db.Column(db.String(32), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
     score = db.Column(db.Float, nullable=False)
     magnitude = db.Column(db.Float, nullable=False)
